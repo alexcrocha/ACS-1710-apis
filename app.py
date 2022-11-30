a2c1e94f92206f121b2af67c4c291e78ad8c5d50
Alex Rocha <rocha.amc@gmail.com>
2022-11-29 21:00:57 -0800

    Complete /results route

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from pprint import PrettyPrinter
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, send_file
-from geopy.geocoders import Nominatim
+
+# from geopy.geocoders import Nominatim
 
 
 ################################################################################
@@ -19,92 +20,98 @@ load_dotenv()
 
 pp = PrettyPrinter(indent=4)
 
-API_KEY = os.getenv('API_KEY')
-API_URL = 'http://api.openweathermap.org/data/2.5/weather'
+API_KEY = os.getenv("API_KEY")
+API_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 
 ################################################################################
 ## ROUTES
 ################################################################################
 
-@app.route('/')
+
+@app.route("/")
 def home():
     """Displays the homepage with forms for current or historical data."""
     context = {
-        'min_date': (datetime.now() - timedelta(days=5)),
-        'max_date': datetime.now()
+        "min_date": (datetime.now() - timedelta(days=5)),
+        "max_date": datetime.now(),
     }
-    return render_template('home.html', **context)
+    return render_template("home.html", **context)
+
 
 def get_letter_for_units(units):
     """Returns a shorthand letter for the given units."""
-    return 'F' if units == 'imperial' else 'C' if units == 'metric' else 'K'
+    return "F" if units == "imperial" else "C" if units == "metric" else "K"
 
-@app.route('/results')
+
+@app.route("/results")
 def results():
     """Displays results for current weather conditions."""
-    # TODO: Use 'request.args' to retrieve the city & units from the query
+    # Use 'request.args' to retrieve the city & units from the query
     # parameters.
-    city = ''
-    units = ''
+    city = request.args.get("city")
+    units = request.args.get("units")
 
     params = {
-        # TODO: Enter query parameters here for the 'appid' (your api key),
+        # Enter query parameters here for the 'appid' (your api key),
         # the city, and the units (metric or imperial).
         # See the documentation here: https://openweathermap.org/current
-
+        "q": city,
+        "appid": API_KEY,
+        "units": units,
     }
 
     result_json = requests.get(API_URL, params=params).json()
 
     # Uncomment the line below to see the results of the API call!
-    # pp.pprint(result_json)
+    pp.pprint(result_json)
 
-    # TODO: Replace the empty variables below with their appropriate values.
+    # Replace the empty variables below with their appropriate values.
     # You'll need to retrieve these from the result_json object above.
 
     # For the sunrise & sunset variables, I would recommend to turn them into
-    # datetime objects. You can do so using the `datetime.fromtimestamp()` 
+    # datetime objects. You can do so using the `datetime.fromtimestamp()`
     # function.
     context = {
-        'date': datetime.now(),
-        'city': '',
-        'description': '',
-        'temp': '',
-        'humidity': '',
-        'wind_speed': '',
-        'sunrise': '',
-        'sunset': '',
-        'units_letter': get_letter_for_units(units)
+        "date": datetime.now(),
+        "city": result_json["name"],
+        "description": result_json["weather"][0]["description"],
+        "temp": result_json["main"]["temp"],
+        "humidity": result_json["main"]["humidity"],
+        "wind_speed": result_json["wind"]["speed"],
+        "sunrise": datetime.fromtimestamp(result_json["sys"]["sunrise"]).strftime(
+            "%H:%M:%S"
+        ),
+        "sunset": datetime.fromtimestamp(result_json["sys"]["sunset"]).strftime(
+            "%H:%M:%S"
+        ),
+        "units_letter": get_letter_for_units(units),
     }
 
-    return render_template('results.html', **context)
+    return render_template("results.html", **context)
 
 
-@app.route('/comparison_results')
+@app.route("/comparison_results")
 def comparison_results():
     """Displays the relative weather for 2 different cities."""
     # TODO: Use 'request.args' to retrieve the cities & units from the query
     # parameters.
-    city1 = ''
-    city2 = ''
-    units = ''
+    city1 = ""
+    city2 = ""
+    units = ""
 
-    # TODO: Make 2 API calls, one for each city. HINT: You may want to write a 
+    # TODO: Make 2 API calls, one for each city. HINT: You may want to write a
     # helper function for this!
-
 
     # TODO: Pass the information for both cities in the context. Make sure to
     # pass info for the temperature, humidity, wind speed, and sunset time!
-    # HINT: It may be useful to create 2 new dictionaries, `city1_info` and 
+    # HINT: It may be useful to create 2 new dictionaries, `city1_info` and
     # `city2_info`, to organize the data.
-    context = {
+    context = {}
 
-    }
-
-    return render_template('comparison_results.html', **context)
+    return render_template("comparison_results.html", **context)
 
 
-if __name__ == '__main__':
-    app.config['ENV'] = 'development'
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.config["ENV"] = "development"
+    app.run(debug=True, port=3000)
